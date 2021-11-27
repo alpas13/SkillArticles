5279d27b13740755753c73c561b1d064b5342b3e
Alexey <alexey@Oleksiys-MBP.fritz.box>
2021-11-27 16:43:45 +0100

    implemented bottombar & submenubehaviors

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/ArticleSubmenu.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/ArticleSubmenu.kt
@@ -7,11 +7,13 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.ViewAnimationUtils
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.animation.doOnEnd
 import androidx.core.animation.doOnStart
 import com.google.android.material.shape.MaterialShapeDrawable
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.extensions.data.dpToPx
+import ru.skillbranch.skillarticles.ui.custom.behaviors.SubmenuBehavior
 import kotlin.math.hypot
 
 /**
@@ -21,7 +23,9 @@ class ArticleSubmenu @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
-) : ConstraintLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr), CoordinatorLayout.AttachedBehavior {
+    override fun getBehavior(): CoordinatorLayout.Behavior<*> = SubmenuBehavior()
+
     private var isOpen: Boolean = false
     private var centerX: Float = context.dpToPx(200)
     private var centerY: Float = context.dpToPx(96)
